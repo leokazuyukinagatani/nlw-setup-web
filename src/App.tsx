af2db9d62e5f03fa45ec3cf9b8665cba252d9c7a
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-01-17 14:31:38 -0300

    feat: config tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './styles/global.css'
+import { Habit } from './components/Habit'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Habit completed={3}/>
+      <Habit completed={23}/>
+      <Habit completed={33}/>
+      <Habit completed={31}/>
+      
       <h1>Hello World</h1>
     </div>
   )
