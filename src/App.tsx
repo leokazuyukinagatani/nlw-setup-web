8d928501eb865c0b134c01011fb1e1148b6b22d6
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-01-18 13:38:43 -0300

    feat: header and summarytable

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './styles/global.css'
-import { Habit } from './components/Habit'
+//import { Habit } from './components/Habit'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
+export function App() {
   return (
-    <div className="App">
-      <Habit completed={3}/>
-      <Habit completed={23}/>
-      <Habit completed={33}/>
-      <Habit completed={31}/>
-      
-      <h1>Hello World</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
