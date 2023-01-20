c1605dab69228715e08da50a03473b063deba5f2
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-01-20 16:02:37 -0300

    feat: integration in habitDay

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -4,14 +4,20 @@ import * as Checkbox from '@radix-ui/react-checkbox'
 
 import clsx from 'clsx'
 import { Check } from 'phosphor-react'
+import dayjs from 'dayjs'
 
 interface HabitDayProps {
-  completed: number
-  amount: number
+  date: Date
+  completed?: number
+  amount?: number
 }
 
-export function HabitDay(props: HabitDayProps) {
-  const completedPercentage = Math.round((props.completed / props.amount) * 100)
+export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
+  const completedPercentage =
+    amount > 0 ? Math.round((completed / amount) * 100) : 0
+
+  const dayAndMonth = dayjs(date).format('DD/MM')
+  const dayOfWeek = dayjs(date).format('dddd')
 
   return (
     <Popover.Root>
@@ -43,8 +49,8 @@ export function HabitDay(props: HabitDayProps) {
       />
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className="font-semibold text-zinc-400 mb-1">terça-feira</span>
-          <span className="font-extrabold leading-tight text-3xl">19/01</span>
+          <span className="font-semibold text-zinc-400 mb-1">{dayOfWeek}</span>
+          <span className="font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900 mb-4" />
           <ProgressBar progress={completedPercentage} />
